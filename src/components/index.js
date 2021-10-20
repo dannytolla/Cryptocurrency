76541cef5ad0a93e46525256dfc6059737286e17
Daniel Tolla <danieltda99@gmail.com>
2021-10-20 10:52:25 +0300

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./Layout/Navbar";
+export { default as Homepage } from "./Layout/Homepage";
+export { default as Exchanges } from "./Layout/Exchanges";
+export { default as Cryptocurrencies } from "./Layout/Cryptocurrencies";
+export { default as CryptoDetails } from "./Layout/CryptoDetails";
+export { default as News } from "./Layout/News";
